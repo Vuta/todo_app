bb7828563c76799fe0241fa67beee3a9c6fedf31
trananhvu <eyelight93@gmail.com>
2018-08-26 17:24:46 +0700

    Deploy Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,11 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :todo_app, TodoAppWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "https://boiling-everglades-65593.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
+
+config :todo_app, TodoApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: 15,
+  ssl: true
 
 # ## SSL Support
 #
@@ -61,4 +69,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
